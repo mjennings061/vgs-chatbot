86898d7d7efedb17be8c2a93f7cdb5dc7d2145e6
mjennings061 <mjennings061@gmail.com>
2023-12-16 14:19:56 +0000

    Chat persistence during the session

--- src/vgs-chatbot/streamlit_app.py
+++ src/vgs-chatbot/streamlit_app.py
@@ -1,12 +1,13 @@
 # streamlit_app.py - GUI for chatbot.
 
 import streamlit as st
-from api import create_docsearch, query_api, DEFAULT_DATA_DIR
+from api import create_docsearch, query_api, DEFAULT_DATA_DIR, PROMPT_PREAMBLE
 
 def app():
     st.title('2FTS Chatbot')
     st.write("Welcome to the 2FTS Chatbot! This chatbot is designed to help you find answers to your questions about 2FTS documentation.")
     st.write("To get started, type your question in the chat window below and press Enter.")
+    st.divider()
 
     # Initialize chat history
     if "messages" not in st.session_state:
@@ -18,21 +19,17 @@ def app():
             st.markdown(message["content"])
 
     # Accept user input.
-    if prompt := st.chat_input(placeholder="Enter your query here..."):
-        # Add user input to chat history.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        # Query QMSBot and display the response.
-        # response = query_qmsbot(prompt, docsearch)
-        response = prompt
-        st.session_state.messages.append({"role": "assistant", "content": response})
-
+    if raw_prompt := st.chat_input(placeholder="Enter your query here..."):
         # Display user input and response.
         with st.chat_message("user"):
-            st.markdown(prompt)
+            st.markdown(raw_prompt)
+
+        # Add prompt preamble.
+        prompt = raw_prompt + PROMPT_PREAMBLE
 
         # Respond to user input.
         with st.chat_message("assistant"):
-            with st.status("Finding answer...", expanded=True) as status:
+            with st.status(label="Finding answer...", expanded=True, state="running") as status:
                 # Create docsearch database.
                 st.write("Fetching vector embeddings...")
                 # Check if docsearch variable exists.
@@ -50,6 +47,10 @@ def app():
 
             # Write response to the chat window as the assistant.
             st.write(response)
+
+        # Add user input to chat history.
+        st.session_state.messages.append({"role": "user", "content": raw_prompt})
+        st.session_state.messages.append({"role": "assistant", "content": response})
 
 
 if __name__ == '__main__':
