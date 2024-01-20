d6aaf3411e09dcf8444707447ac12794e06321e8
mjennings061 <mjennings061@gmail.com>
2024-01-20 14:12:45 +0000

    PEP8 tidy-up

--- src/vgs-chatbot/streamlit_app.py
+++ src/vgs-chatbot/streamlit_app.py
@@ -6,7 +6,11 @@ from api import create_docsearch, query_api, PROMPT_PREAMBLE, database_exists
 
 def fetch_response(prompt):
     """Fetch response from the vgs-chatbot"""
-    with st.status(label="Finding answer...", expanded=True, state="running") as status:
+    with st.status(
+        label="Finding answer...",
+        expanded=True,
+        state="running"
+    ) as status:
         # Create docsearch database.
         st.write("Fetching vector embeddings...")
         # Check if docsearch variable exists.
@@ -19,7 +23,7 @@ def fetch_response(prompt):
         # Query VGS bot and display the response.
         st.write("Querying VGS Bot...")
         response = query_api(prompt, st.session_state.docsearch)
-        
+
         # Collapse status message.
         status.update(label="Complete!", state="complete", expanded=False)
 
@@ -29,12 +33,22 @@ def fetch_response(prompt):
 def form_prompt_with_context(user_input, chat_history):
     """Form prompt with the entire conversation history as context."""
     # Get all comments from the user in the chat history.
-    user_history = [message["content"] for message in chat_history if message["role"] == "user"]
+    user_history = [
+        message["content"]
+        for message in chat_history if message["role"] == "user"
+    ]
 
     # Join user history into a single string.
     if len(user_history) > 0:
         user_history = "\n".join(user_history)
-        context = f"Previous questions for context:```\n{user_history}```\n\nNew question:\n"
+        context = f"""
+            Previous questions for context:
+
+            ```
+            {user_history}
+            ```
+
+            New question:\n"""
     else:
         context = ""
 
@@ -45,8 +59,11 @@ def form_prompt_with_context(user_input, chat_history):
 
 def app():
     st.title('2FTS Chatbot')
-    st.write("""Welcome to the 2FTS Chatbot! This chatbot is designed to help you find answers to your questions about 2FTS documentation.
-                To get started, type your question in the chat window below and press Enter.""")
+    st.write("""Welcome to the 2FTS Chatbot!
+             This chatbot is designed to help you find answers to your
+             questions about 2FTS documentation.
+             To get started, type your question in the chat window
+             below and press Enter.""")
     st.divider()
 
     # Initialize chat history.
@@ -77,14 +94,20 @@ def app():
             st.write(response)
 
         # Add user input to chat history.
-        st.session_state.chat_history.append({"role": "user", "content": raw_prompt})
-        st.session_state.chat_history.append({"role": "assistant", "content": response})
+        st.session_state.chat_history.append({
+            "role": "user",
+            "content": raw_prompt
+        })
+        st.session_state.chat_history.append({
+            "role": "assistant",
+            "content": response
+        })
 
     # Display contact information.
     with st.sidebar:
         st.markdown("""
             ## Contact Information
-            For any queries or support, please contact us at 
+            For any queries or support, please contact us at \n
             [michael.jennings100@rafac.mod.gov.uk](mailto:michael.jennings100@rafac.mod.gov.uk)
         """)
 
